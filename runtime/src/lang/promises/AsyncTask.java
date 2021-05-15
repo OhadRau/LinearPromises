b61c73dc7fe6b800ff04809c2b6dc490fe8c9dc9
ohadrau <ohadrau@yahoo.com>
2021-05-15 16:09:26 -0400

    runtime/AsyncTask: use Function0<Unit> to back AsyncTask
    Previously, AsyncTask was backed by a Runnable, which is a lambda of
    void(). This was semantically incorrect, because async expected a
    Unit() lambda. In fact, this could allow us to generate incorrect code
    in certain situations:
    
        async (if x then () else () end)
    
    would generate:
    
        new AsyncTask(() -> ((x) ? Unit.the : Unit.the))
    
    which would fail to compile in Java. Interestingly, Java allows for:
    
        new AsyncTask(() -> Unit.the)
    
    which hid the error until now.

--- runtime/src/lang/promises/AsyncTask.java
+++ runtime/src/lang/promises/AsyncTask.java
@@ -6,13 +6,13 @@ public class AsyncTask {
     private static long free_id = 0;
     private static ThreadLocal<AsyncTask> threadTask = new ThreadLocal<>();
     private long id;
-    private Runnable runnable;
+    private Function0<Unit> runnable;
 
     public static AsyncTask currentTask() {
         return threadTask.get();
     }
 
-    public AsyncTask(Runnable runnable) {
+    public AsyncTask(Function0<Unit> runnable) {
         this.id = free_id++;
         this.runnable = runnable;
     }
@@ -20,7 +20,7 @@ public class AsyncTask {
     public void run(ExecutorService service) {
         service.execute(() -> {
             threadTask.set(this);
-            this.runnable.run();
+            this.runnable.apply();
         });
     }
 
